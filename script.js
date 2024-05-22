bdf2d0f4fd8ba0c7fbf927e03a43a798e6f5d33d
Ag <theankushgautam@gmail.com>
2024-05-22 22:14:04 +0545

    Fix 16x16 grid using flex only

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const container = document.querySelector("#container");
 
 //creating a 16x16 grid
 for (let i = 0; i < 16; i++) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+
   for (let j = 0; j < 16; j++) {
     const grid = document.createElement("div");
     grid.classList.add("grid-item");
-    container.appendChild(grid);
+    row.appendChild(grid);
   }
+
+  container.appendChild(row);
 }
