3bc557ab373c849ec1b13ff1b6a9f3a2e36bac01
Ag <theankushgautam@gmail.com>
2024-05-22 22:33:22 +0545

    Dynamic change of grid by user input

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const container = document.querySelector("#container");
+const promptBtn = document.querySelector("#prompt-btn");
+let input = 16; //by default 16
 
-//creating a 16x16 grid
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
+//validating user input
+function getUserInput() {
+  do {
+    input = prompt("Enter no. of squares(1-100): ");
+  } while (isNaN(input) || input < 1 || input > 100);
 
-  for (let j = 0; j < 16; j++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid-item");
-    row.appendChild(grid);
-  }
-
-  container.appendChild(row);
+  return input;
 }
+
+//generate grid
+function generateGrid(squares) {
+  for (let i = 0; i < squares; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < squares; j++) {
+      const grid = document.createElement("div");
+      grid.classList.add("grid-item");
+      row.appendChild(grid);
+    }
+
+    container.appendChild(row);
+  }
+}
+
+promptBtn.addEventListener("click", () => {
+  generateGrid(getUserInput());
+});
+
+generateGrid(input);
